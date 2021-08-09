49f2b6fc7b4dafa6fdcaa0f42ce9e419edd8b4d9
fukky21 <r.fukky21@gmail.com>
2021-08-09 15:58:02 +0900

    feat: トップページを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,23 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div align="center">
+    <v-img src="/logo.png" width="40vw" max-width="2084" aspect-ratio="1"></v-img>
+    <v-container>
+      <v-row justify="center">
+        <v-btn x-large color="primary ma-4" width="80vw" max-width="600px">あそぶ</v-btn>
+      </v-row>
+      <v-row justify="center">
+        <v-btn x-large color="white ma-4" width="80vw" max-width="600px" >あそびかた</v-btn>
+      </v-row>
+    </v-container>
+    <v-footer absolute color="background">
+      <v-row>
+        <v-col align="left">
+          <span class="blue-grey--text">developed by fukky21</span>
+        </v-col>
+        <v-col align="right">
+          お問い合わせは<a>こちら</a>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </div>
 </template>
