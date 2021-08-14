3a653ea1c387acf72ef2499b116a642bc6019d93
fukky21 <r.fukky21@gmail.com>
2021-08-15 00:59:02 +0900

    feat: このアプリについてページを追加

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-app-bar app fixed class="white">
+      <v-app-bar-nav-icon @click="back()">
+        <v-icon large color="black">mdi-chevron-left</v-icon>
+      </v-app-bar-nav-icon>
+      <v-app-bar-title>このアプリについて</v-app-bar-title>
+    </v-app-bar>
+    <v-main class="background">
+      <v-container>
+        This is About Page.
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class AboutPage extends Vue {
+  back(): void {
+    this.$router.push('/');
+  }
+}
+</script>
