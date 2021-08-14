9ed867ea19285d22dab98b7737083581a837ab46
fukky21 <r.fukky21@gmail.com>
2021-08-14 17:16:06 +0900

    fix: タイトル画面を修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,31 @@
 <template>
-  <div align="center">
-    <v-img src="/logo.png" width="40vw" max-width="2084" aspect-ratio="1"></v-img>
-    <v-container>
-      <v-row justify="center">
-        <v-btn href="play" x-large class="primary ma-4 font-weight-bold" width="80vw" max-width="600px">あそぶ</v-btn>
-      </v-row>
-      <v-row justify="center">
-        <v-btn x-large class="white ma-4 font-weight-bold" width="80vw" max-width="600px" >あそびかた</v-btn>
-      </v-row>
-    </v-container>
-    <v-footer absolute color="background">
-      <v-row>
-        <v-col align="left">
-          <span class="blue-grey--text">developed by fukky21</span>
-        </v-col>
-        <v-col align="right">
-          お問い合わせは<a>こちら</a>
-        </v-col>
-      </v-row>
-    </v-footer>
+  <div>
+    <v-row justify="center" align-content="center" style="height: 95vh;">
+      <v-col cols="12" align="center">
+        <v-card class="d-flex align-center justify-center pa-4 mx-auto rounded-circle" max-width="450" height="10vh" min-height="50" max-height="150" outlined>
+          <div>
+            あなたの代わりに話題を考えます！
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" align="center">
+        <v-container class="pa-4">
+          <v-img src="/logo.png" height="40vh" min-height="50" max-height="300" aspect-ratio="1" contain></v-img>
+        </v-container>
+      </v-col>
+      <v-col cols="12" align="center">
+        <v-container>
+          <v-row justify="center">
+            <v-btn href="play" x-large class="primary ma-4 font-weight-bold" width="80vw" max-width="600px" height="5vh" max-height="50">あそぶ</v-btn>
+          </v-row>
+          <v-row justify="center">
+            <v-btn x-large class="white ma-4 font-weight-bold" width="80vw" max-width="600px" height="5vh" max-height="50">このアプリについて</v-btn>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+    <div style="height: 5vh; text-align: center">
+      <span>developed by fukky21</span>
+    </div>
   </div>
 </template>
