b0cfa0bdf3edc7a2dca3b41b54abae76131df4cd
fukky21 <r.fukky21@gmail.com>
2021-08-15 04:57:17 +0900

    feat: このアプリについて画面を作成

--- pages/about.vue
+++ pages/about.vue
@@ -8,7 +8,25 @@
     </v-app-bar>
     <v-main class="background">
       <v-container>
-        This is About Page.
+        <h2># あそびかた</h2>
+        <v-container>
+          <v-img :src="require(`~/assets/images/screenshot.png`)" height="50vw" max-height="400" style="border: solid;" contain></v-img>
+          <div class="ma-4">
+            <p>「次のお題」ボタンを押すと、ヨッチーがお題を考えはじめます。</p>
+            <p>数秒のアニメーションの後、お題が表示されます。</p>
+            <p>お題は毎回ランダムに決定されます。</p>
+            <p>終了したいときは「おしまい」ボタンを押してください。トップ画面に戻ります。</p>
+          </div>
+        </v-container>
+        <h2># お問い合わせ</h2>
+        <v-container>
+          <div class="ma-4">
+            <p>お問い合わせは以下のボタンからお願いします。</p>
+            <p>不具合やご要望など、ご連絡いただけるとありがたいです。</p>
+            <p>「こんなお題を追加して欲しい！」などのご意見もお待ちしております。</p>
+            <v-btn :href="googleFormURL" x-large class="primary ma-4 font-weight-bold" target="_blank">お問い合わせフォーム</v-btn>
+          </div>
+        </v-container>
       </v-container>
     </v-main>
   </v-app>
@@ -19,6 +37,8 @@ import { Component, Vue } from 'nuxt-property-decorator';
 
 @Component
 export default class AboutPage extends Vue {
+  googleFormURL = 'https://docs.google.com/forms/d/e/1FAIpQLSetLQIcyNt628Er-HqRlPGZKZP97a095m7O8lvtUNO2dv4S8Q/viewform?usp=sf_link';
+
   back(): void {
     this.$router.push('/');
   }
