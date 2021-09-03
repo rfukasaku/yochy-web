5236ab1a72c22f6e4b334006dcd6d3e155a6f8a7
fukky21 <r.fukky21@gmail.com>
2021-09-03 23:31:52 +0900

    feat: プライバシーポリシーを追加

---
+++ pages/privacy_policy.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main class="background">
+      <v-container>
+        <h1>ヨッチー プライバシーポリシー</h1>
+        <v-container>
+          <h2># データの収集について</h2>
+          <div class="ma-4">
+            <p>本アプリでは、個人情報の収集は一切行っておりません。</p>
+          </div>
+        </v-container>
+        <v-container>
+          <h2># プライバシーポリシーの改定について</h2>
+          <div class="ma-4">
+            <p>本プライバシーポリシーの内容は予告なく変更される場合があります。</p>
+            <p>変更が生じた場合はサイト上に掲示することで、利用者の皆様へお知らせいたします。</p>
+          </div>
+        </v-container>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
