b65887860b94b902f309397efb01353f97fb9d40
fukky21 <r.fukky21@gmail.com>
2021-09-13 04:14:00 +0900

    fix: 画面デザインを修正

--- pages/privacy_policy.vue
+++ pages/privacy_policy.vue
@@ -1,18 +1,20 @@
 <template>
   <v-app>
+    <v-app-bar app fixed class="white">
+      <v-toolbar-title>プライバシーポリシー</v-toolbar-title>
+    </v-app-bar>
     <v-main class="background">
-      <v-container>
-        <h1>ヨッチー プライバシーポリシー</h1>
-        <v-container>
-          <h2># データの収集について</h2>
-          <div class="ma-4">
+      <v-container class="px-8">
+        <v-container class="my-4">
+          <h2>個人情報の収集について</h2>
+          <div class="mt-4">
             <p>本アプリでは、個人情報の収集は一切行っておりません。</p>
           </div>
         </v-container>
-        <v-container>
-          <h2># プライバシーポリシーの改定について</h2>
-          <div class="ma-4">
-            <p>本プライバシーポリシーの内容は予告なく変更される場合があります。</p>
+        <v-container class="my-4">
+          <h2>プライバシーポリシーの改定について</h2>
+          <div class="mt-4">
+            <p>プライバシーポリシーの内容は予告なく変更される場合があります。</p>
             <p>変更が生じた場合はサイト上に掲示することで、利用者の皆様へお知らせいたします。</p>
           </div>
         </v-container>
